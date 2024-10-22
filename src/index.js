0237a5dcac7411e0f8bb02127845f0d7ed28b287
yuriiavr <yurii.avramets@gmail.com>
2024-10-23 01:27:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter basename="/luna/">
+      <HashRouter basename="/">
         <App />
       </HashRouter>
     </Provider>
